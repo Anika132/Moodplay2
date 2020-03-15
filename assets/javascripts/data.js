4e5c8223fffe3848e73b80b22a204582f354d7a7
Anika Sharma <35227081+Anika132@users.noreply.github.com>
2020-03-15 16:59:54 +0530

    Update data.js

--- assets/javascripts/data.js
+++ assets/javascripts/data.js
@@ -1,21 +1,21 @@
 // Js for getting data from the API basically pass the category(paarameter) for which you want to get the songs
 
-function doAjax(parameter){
-    var pr = new Promise((resolve,reject)=>{
-        const url =`https://itunes.apple.com/search?term=${parameter}&media=music`;
-        fetch(url,{
-            method: "get",
-            mode:"no-cors",
-            headers: 
-            {
-                 "Access-Control-Allow-Origin": "https://anika132.github.io",
-                 "Content-Type": "application/json"
-            }
-      }).then(response=>response.json().then(json=>{
-           resolve(json.results); 
-        })).catch(err=>console.log(err));
-    });
-    return pr;
-}    
+// function doAjax(parameter){
+//     var pr = new Promise((resolve,reject)=>{
+//         const url =`https://itunes.apple.com/search?term=${parameter}&media=music`;
+//         fetch(url,{
+//             method: "get",
+//             mode:"no-cors",
+//             headers: 
+//             {
+//                  "Access-Control-Allow-Origin": "https://anika132.github.io",
+//                  "Content-Type": "application/json"
+//             }
+//       }).then(response=>response.json().then(json=>{
+//            resolve(json.results); 
+//         })).catch(err=>console.log(err));
+//     });
+//     return pr;
+// }    
 
     
