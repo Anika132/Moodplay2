73c3433f76c4f99cd317c4c7ab55aae77ad5a355
Anika Sharma <35227081+Anika132@users.noreply.github.com>
2020-03-15 17:06:41 +0530

    Update data.js

--- assets/javascripts/data.js
+++ assets/javascripts/data.js
@@ -3,15 +3,7 @@
 function doAjax(parameter){
     var pr = new Promise((resolve,reject)=>{
         const url =`https://itunes.apple.com/search?term=${parameter}&media=music`;
-        fetch(url,{
-            method: "get",
-            mode:"no-cors",
-            headers: 
-            {
-                 "Access-Control-Allow-Origin": "https://anika132.github.io",
-                 "Content-Type": "application/json"
-            }
-      }).then(response=>response.json().then(json=>{
+        fetch(url).then(response=>response.json().then(json=>{
            resolve(json.results); 
         })).catch(err=>console.log(err));
     });
